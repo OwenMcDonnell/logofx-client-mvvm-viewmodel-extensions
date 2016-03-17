d27d2cd9f53ffb2fe2106d6bfebf679fb562894a
godrose <godrose@gmail.com>
2016-03-17 09:09:23 +0200

    Initial commit.

---
+++ src/LogoFX.Client.Mvvm.ViewModel.Extensions.Tests/TestBootstrapper.cs
@@ -0,0 +1,17 @@
+﻿using LogoFX.Client.Bootstrapping;
+using LogoFX.Client.Bootstrapping.Adapters.SimpleContainer;
+
+namespace LogoFX.Client.Mvvm.ViewModel.Extensions.Tests
+{    
+    class TestBootstrapper : BootstrapperContainerBase<TestConductorViewModel,ExtendedSimpleContainerAdapter>
+    {
+        public TestBootstrapper(ExtendedSimpleContainerAdapter container)
+            :base(container, new BootstrapperCreationOptions
+            {
+                UseApplication = false
+            })
+        {
+            
+        }
+    }
+}
