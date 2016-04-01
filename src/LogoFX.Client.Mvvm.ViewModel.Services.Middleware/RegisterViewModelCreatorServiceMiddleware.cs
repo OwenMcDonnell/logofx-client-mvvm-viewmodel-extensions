d995ecb5f5df0c4e8795fd075c2fa6b985f303bf
godrose <godrose@gmail.com>
2016-04-01 12:02:01 +0300

    Made the middleware public

--- src/LogoFX.Client.Mvvm.ViewModel.Services.Middleware/RegisterViewModelCreatorServiceMiddleware.cs
+++ src/LogoFX.Client.Mvvm.ViewModel.Services.Middleware/RegisterViewModelCreatorServiceMiddleware.cs
@@ -5,11 +5,21 @@ using Solid.Practices.Middleware;
 
 namespace LogoFX.Client.Mvvm.ViewModel.Services
 {
-    class RegisterViewModelCreatorServiceMiddleware<TRootViewModel, TIocContainerAdapter> :
+    /// <summary>
+    /// Middleware that's responsbile for registering <see cref="IViewModelCreatorService"/> into the ioc container adapter.
+    /// </summary>
+    /// <typeparam name="TRootViewModel">The type of the root view model.</typeparam>
+    /// <typeparam name="TIocContainerAdapter">The type of the ioc container adapter.</typeparam>    
+    public class RegisterViewModelCreatorServiceMiddleware<TRootViewModel, TIocContainerAdapter> :
         IMiddleware<IBootstrapperWithContainerAdapter<TRootViewModel, TIocContainerAdapter>> 
         where TRootViewModel : class 
         where TIocContainerAdapter : class, IIocContainer, IIocContainerAdapter, IBootstrapperAdapter, new()
     {
+        /// <summary>
+        /// Applies the middleware on the specified object.
+        /// </summary>
+        /// <param name="object">The object.</param>
+        /// <returns></returns>
         public IBootstrapperWithContainerAdapter<TRootViewModel, TIocContainerAdapter> Apply(
             IBootstrapperWithContainerAdapter<TRootViewModel, TIocContainerAdapter> @object)
         {
