8ad2d860aade0d716e8b1a775f1e2748d882e754
Gennady Verdel <godrose@gmail.com>
2017-07-18 12:14:51 +0300

    Updated assembly versions

--- src/LogoFX.Client.Mvvm.ViewModel.Extensions/uwp/Properties/AssemblyInfo.cs
+++ src/LogoFX.Client.Mvvm.ViewModel.Extensions/uwp/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("LogoFX.Client.Mvvm.ViewModel.Extensions.UWP")]
+[assembly: AssemblyTitle("LogoFX.Client.Mvvm.ViewModel.Extensions")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("LogoFX.Client.Mvvm.ViewModel.Extensions.UWP")]
+[assembly: AssemblyProduct("LogoFX.Client.Mvvm.ViewModel.Extensions")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -23,7 +23,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0")]
+[assembly: AssemblyFileVersion("1.1.0")]
 [assembly: ComVisible(false)]
 
