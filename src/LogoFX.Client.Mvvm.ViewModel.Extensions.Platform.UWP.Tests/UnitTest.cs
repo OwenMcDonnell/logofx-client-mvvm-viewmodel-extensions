69fd315e07eacd3e8d3c89e53021a21688cef083
Gennady Verdel <godrose@gmail.com>
2017-12-17 10:19:56 +0200

    Added uwp unit tests app - inactive for now

---
+++ src/LogoFX.Client.Mvvm.ViewModel.Extensions.Platform.UWP.Tests/UnitTest.cs
@@ -0,0 +1,12 @@
+﻿using Xunit;
+
+namespace LogoFX.Client.Mvvm.ViewModel.Extensions.Platform.UWP.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+        }
+    }
+}
