227ca1b52728a15df774ff91350c8c93bfdee379
dmitrybublik <dmitry.buba@gmail.com>
2018-04-07 21:18:45 +0300

    Added some functionality to VirtualContainerViewModel

--- src/LogoFX.Client.Mvvm.ViewModel.Extensions/src/VirtualContainerViewModel.cs
+++ src/LogoFX.Client.Mvvm.ViewModel.Extensions/src/VirtualContainerViewModel.cs
@@ -6,17 +6,81 @@ namespace iXCapture.Processing.ViewModels
     public abstract class VirtualContainerViewModel<TModel, TViewModel> : Conductor<TViewModel>, IModelWrapper<TModel>, IHaveSubViewModel
         where TViewModel : class, IModelWrapper<TModel>
     {
+        #region Fields
+
         private bool _isSubViewModelVisible;
         private TViewModel _subViewModel;
+
+        #endregion
+
+        #region Constructors
 
         protected VirtualContainerViewModel(TModel model)
         {
             Model = model;
         }
 
+        #endregion
+
+        #region Public Properties
+
+        private bool _isSelected;
+        public bool IsSelected
+        {
+            get { return _isSelected; }
+            set { Set(ref _isSelected, value); }
+        }
+
+        public TViewModel SubViewModel
+        {
+            get { return GetSubViewModel(); }
+        }
+        
+        #endregion
+        
+        #region Protected
+
+        protected abstract TViewModel CreateSubViewModel();
+
+        #endregion
+
+        #region Private
+
+        private TViewModel GetSubViewModel()
+        {
+            return _subViewModel ?? (_subViewModel = CreateSubViewModel());
+        }
+
+        private void UpdateSubModel()
+        {
+            ActivateItem(IsSubViewModelVisible ? GetSubViewModel() : null);
+        }
+
+        #endregion
+
+        #region Overrides
+
+        protected override void OnActivate()
+        {
+            base.OnActivate();
+
+            if (IsSubViewModelVisible)
+            {
+                UpdateSubModel();
+            }
+        }
+
+        #endregion
+
+        #region IModelWrapper<TModel>
+
         object IModelWrapper.Model => Model;
 
         public TModel Model { get; private set; }
+
+        #endregion
+
+        #region IHaveSubViewModel
 
         public bool IsSubViewModelVisible
         {
@@ -30,16 +94,6 @@ namespace iXCapture.Processing.ViewModels
             }
         }
 
-        protected abstract TViewModel CreateSubViewModel();
-
-        private TViewModel GetSubViewModel()
-        {
-            return _subViewModel ?? (_subViewModel = CreateSubViewModel());
-        }
-
-        private void UpdateSubModel()
-        {
-            ActivateItem(IsSubViewModelVisible ? GetSubViewModel() : null);
-        }
+        #endregion
     }
 }
